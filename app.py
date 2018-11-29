33d40b8437de8531e67f66d64f3d1b47a127dbf6
Cynthia Vint <cynthiavint@Cynthias-MacBook-Pro.local>
2018-11-29 17:53:40 -0500

    Initial app boilerplate

---
+++ app.py
@@ -0,0 +1,59 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from tingmo import TingMo
+from dash.dependencies import Input, Output, State
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+server = app.server
+
+app.layout = html.Div([
+    html.H1('TingMo',style={'text-align':'center','font-size':'50px'}),
+    html.Div(children=[
+    html.Div(children=[
+        html.Div(children=[
+
+                html.H2('What is TingMo?'),
+                html.P('TingMo is is powerful tool designed to quickly identify lookalike domains.')
+
+        ], className='six columns'),
+        html.Div(children=[
+            html.H4('Query Options'),
+            dcc.Checklist(id='brands',
+            options=[
+                {'label': 'Remove brand TLD matches', 'value': 'BRD'}
+            ],
+            values=['BRD']),
+            html.H4('Enter Domains to Check (comma-separated)'),
+            dcc.Textarea(value='google.com', id='dom_list',maxLength=20000,
+                     style = {'width': '48%'}),
+            html.Div(html.Button('Submit', id='button')),
+            html.P(id='my-div',children='Results appear here.')],
+        className='six columns',
+        )])],className='row')])
+
+
+@app.callback(
+    Output(component_id='my-div', component_property='children'),
+    [Input('button', 'n_clicks'),Input('brands','values')],
+    [State('dom_list', 'value')]
+)
+def get_matches(n_clicks,values,domain_list):
+    domain_list = ['http://'+d.strip().lower() for d in domain_list.split(',')]
+    exclude = 'BRD' in values
+    results = tm.query_LSH(domain_list,exclude)
+    html_output = []
+    for r in results:
+        html_output.append(html.H3(children=r+':'))
+        if len(results[r]) == 0:
+            html_output.append(html.P(children='No results.'))
+        for result in results[r]:
+            html_output.append(html.P(children=result))
+    return html_output
+
+if __name__ == '__main__':
+    tm = TingMo()
+    app.run_server(debug=True,use_reloader=False)
---
+++ tingmo.py
@@ -0,0 +1,150 @@
+from datasketch import MinHash, MinHashLSH
+from tld import get_tld
+from datetime import datetime,timedelta
+import zipfile
+import os
+import pandas as pd
+from bs4 import BeautifulSoup
+import requests
+import re
+
+class TingMo:
+
+    def __init__(self,newly_registered_url='https://whoisds.com/newly-registered-domains/',n_days=1):
+
+        self._download_domains(newly_registered_url,n_days)
+        self.new_domains = self._read_clean_domains()
+        self.brand_tlds = self.fetch_brand_tlds()
+        self.lsh = self._train_LSH()
+
+    # help from https://github.com/gfek/Hunting-New-Registered-Domains/blob/master/hnrd.py
+    def _download_domains(self,url, n_days=1):
+
+        # get table of dates with urls
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text,features='lxml')
+        table = soup.find('table')
+
+        # convert to pd df and clean
+        link_df = pd.read_html(str(table))[0]
+        link_df = link_df.iloc[1:,]
+        link_df.columns = ['date', 'count', 'creation_date', 'download_button']
+
+        # extract hrefs for download
+        links = re.findall('href=\"([^>]*)\"',str(table))
+        link_df['url'] = links
+
+        for i in range(min(n_days,link_df.shape[0])):
+
+            # get date label
+            d = datetime.now() - timedelta(days=i)
+            d = d.strftime('%Y-%m-%d')
+
+            if not os.path.isfile('./new_domains/' + d):
+                nrd_zip = link_df.iloc[i,]['url']
+                try:
+                    resp = requests.get(nrd_zip, stream=True)
+
+                    print("Downloading File {} - Size {}...".format(d + '.zip', resp.headers['Content-length']))
+                    if resp.headers['Content-length']:
+                        with open(d + ".zip", 'wb') as f:
+                            for data in resp.iter_content(chunk_size=1024):
+                                f.write(data)
+                        try:
+                            zip = zipfile.ZipFile(d + ".zip")
+                            zip.extractall(path='new_domains/'+d,)
+                        except:
+                            print("File is not a zip file.")
+                except:
+                    print("File {}.zip does not exist on the remote server.".format(d))
+
+    # read in newly registered domains, remove brand TLD domains
+    def _read_clean_domains(self,n_days=1):
+
+        domains = []
+
+        for i in range(n_days):
+
+            # get date label
+            d = datetime.now() - timedelta(days=i)
+            d = d.strftime('%Y-%m-%d')
+
+            with open('new_domains/' + d + '/domain-names.txt') as f:
+                domains += f.readlines()
+
+        domains = [dom.strip().lower() for dom in domains]
+
+        return domains
+
+    # fetch brand TLDs
+    def fetch_brand_tlds(self, tld_url = 'https://dotbrandobservatory.com/dashboard/dot-brand-dashboard/'):
+
+        # read in table
+        tld = requests.get(tld_url)
+        tld = pd.read_html(tld.text)[0]
+        tld.columns = ['tld', 'Contract', 'Country', 'Delegation', 'Domains',
+                       'who_knows', 'Vertical/Industry', 'Websites']
+
+        return [t.lower().strip() for t in tld['tld']]
+
+    def _train_LSH(self):
+
+        # create LSH model
+        lsh = MinHashLSH(weights=(.5, .5))
+
+        # train LSH model
+        for dom in self.new_domains[:1000]:
+
+            # remove TLD
+            tld_info = get_tld('http://'+dom, as_object=True, fail_silently=True)
+            if tld_info is None:
+                continue
+
+            d = tld_info.domain
+
+            # ignore super short new domains
+            if len(d) <= 3:
+                continue
+
+            # create bigram set
+            bigrams = [d[i:i+2] for i in range(len(d) - 1)]
+            bigrams = set(bigrams)
+
+            # create and update minhash obj
+            m = MinHash()
+            for b in bigrams:
+                m.update(b.encode('utf-8'))
+
+            lsh.insert(dom,m)
+
+        print('LSH Trained!')
+        return lsh
+
+    def query_LSH(self, domain_list, exclude=False):
+
+        matches = {}
+
+        for domain in domain_list:
+
+            tld_info = get_tld(domain, as_object=True)
+            just_dom = tld_info.domain
+
+            bigrams = set([just_dom[i:i+2] for i in range(len(just_dom) - 1)])
+
+            m = MinHash()
+            for b in bigrams:
+                m.update(b.encode('utf-8'))
+
+            match = self.lsh.query(m)
+
+            if exclude:
+                for item in match:
+                    match = [i for i in item if get_tld(i) not in self.brand_tlds]
+
+            # this part will remove matches with brand tlds later
+
+            matches[just_dom] = match
+
+        return matches
+
+
