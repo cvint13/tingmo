d0f00f628dd987d8b2f6fb3ca67390c9814e7645
Cynthia Vint <cynthiavint@Cynthias-MacBook-Pro.local>
2018-11-30 07:26:01 -0500

    KISS

--- app.py
+++ app.py
@@ -3,24 +3,13 @@ import dash_core_components as dcc
 import dash_html_components as html
 from tingmo import TingMo
 from dash.dependencies import Input, Output, State
-import time
-from webapp2 import WSGIApplication
-import multiprocessing
-from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
 
 # number of seconds between re-calculating the data
 UPDATE_INTERVAL = 86400
 
-def get_new_data(period=UPDATE_INTERVAL):
+def get_new_data():
     global tm
     tm = TingMo()
-
-def get_new_data_every(period=UPDATE_INTERVAL):
-    """Update the data every 'period' seconds"""
-    while True:
-        get_new_data()
-        print("data updated")
-        time.sleep(period)
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -74,25 +63,6 @@ def get_matches(n_clicks,values,domain_list):
         for result in results[r]:
             html_output.append(html.P(children=result))
     return html_output
-
-# Run the function in another thread
-executor = ThreadPoolExecutor(max_workers=1)
-executor.submit(get_new_data_every)
-
-workers=1 + (multiprocessing.cpu_count() * 2)
-gunicorn = WSGIApplication()
-gunicorn.load_wsgiapp = lambda: app
-gunicorn.cfg.set('workers', workers)
-gunicorn.cfg.set('threads', workers)
-gunicorn.cfg.set('pidfile', None)
-gunicorn.cfg.set('worker_class', 'sync')
-gunicorn.cfg.set('keepalive', 10)
-gunicorn.cfg.set('accesslog', '-')
-gunicorn.cfg.set('errorlog', '-')
-gunicorn.cfg.set('reload', True)
-gunicorn.chdir()
-gunicorn.run()
-
 
 if __name__ == '__main__':
     app.run_server(debug=True,use_reloader=False)
